aa6b0b5f1c3d7b53241951097ca7bed05c53af2a
Roger Franco <rogerfrancogalt@gmail.com>
2023-02-20 11:13:44 -0400

    remove extension from url

--- index.js
+++ index.js
@@ -5,6 +5,18 @@ const app = express();
 const publicPath=path.join(__dirname,'public')
 // console.log(publicPath);
 
-app.use(express.static(publicPath));
+// app.use(express.static(publicPath));
+app.get('', (req, res) => {
+  res.sendFile(`${publicPath}/index.html`)
+})
+app.get('/about', (req, res) => {
+  res.sendFile(`${publicPath}/about.html`)
+})
+app.get('/help', (req, res) => {
+  res.sendFile(`${publicPath}/help.html`)
+})
+app.get('*', (req, res) => {
+  res.sendFile(`${publicPath}/nopage.html`)
+})
 
 app.listen(5500);
