3120a573edd459dc64ea8fec18a73fbc8ad9485d
Roger Franco <rogerfrancogalt@gmail.com>
2023-02-20 09:35:36 -0400

    Render html e json

---
+++ 1-rascunho/express/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+
+app.get("", (req, resp) => {
+    console.log(req.query.name)
+    resp.send("Welcome,"+req.query.name);
+});
+
+app.get("/about", (req, resp) => {
+    resp.send("Welcome, This is a About Page");
+});
+
+app.get("/help", (req, resp) => {
+    resp.send("Welcome, This is a Help Page");
+});
+
+
+app.listen(5500);
+
+//  http://localhost:5500/?name=roger
--- index.js
+++ index.js
@@ -2,16 +2,32 @@ const express = require('express');
 const app = express();
 
 app.get("", (req, resp) => {
-    console.log(req.query.name)
-    resp.send("Welcome,"+req.query.name);
+    resp.send(`
+    <h1>Welcome to Home Page</h1>
+    <a href="/about">Go to About page</a>
+    `);
 });
 
 app.get("/about", (req, resp) => {
-    resp.send("Welcome, This is a About Page");
+    resp.send(`
+    <input type="text"placeholder="User name" value="${req.query.name}" />
+    <button>Click</button>
+    <a href="/">Go to Home page</a>
+    `);
+    // http://localhost:5500/about/?name=Roger
 });
 
 app.get("/help", (req, resp) => {
-    resp.send("Welcome, This is a Help Page");
+    resp.send([
+      {
+        name: 'Roger',
+        email: 'roger@roger.com',
+      },
+      {
+        name: 'Thu',
+        email: 'thu@thu.com',
+      }
+    ]);
 });
 
 
