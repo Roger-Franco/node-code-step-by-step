666b02220a54f272784891481052741d815a37cd
Roger Franco <rogerfrancogalt@gmail.com>
2023-02-22 11:20:15 -0400

    separando o arquivo mongodb em um local apropriado

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
-const {MongoClient} = require('mongodb')
-const url = 'mongodb://localhost:27017';
-const databaseName = 'e-comm';
-const client = new MongoClient(url)
+const dbConnect= require('./mongodb')
 
-async function getData() {
-  let result = await client.connect();
-  db = result.db(databaseName);
-  collection = db.collection('products');
-  let data = await collection.find({}).toArray();
+
+const main =  async () => {
+  let data = await dbConnect();
+  data = await data.find({name:'nord'}).toArray();
   console.log(data);
 }
-
-getData()
+main()
