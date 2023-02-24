239a3f8b88508c4b85daca7a5ddf872c88039189
Roger Franco <rogerfrancogalt@gmail.com>
2023-02-24 10:10:00 -0400

    CRUD with Mongoose

--- index.js
+++ index.js
@@ -1,15 +1,61 @@
 const mongoose= require('mongoose')
 
+mongoose.connect('mongodb://localhost:27017/e-comm');
+const productSchema = new mongoose.Schema({ // => aqui no schema eu preciso colocar todas as chaves que irao para o db.
+  name: String,
+  price: Number,
+  brand: String,
+  category: String
+})
 
-const main =  async () => {
-  mongoose.connect('mongodb://localhost:27017/e-comm');
-  const productSchema = new mongoose.Schema({ // => aqui no schema eu preciso colocar todas as chaves que irao para o db.
-    name: String,
-    price: Number
-  })
+const saveInDB =  async () => {
   const Product = mongoose.model('products', productSchema)
-  let data = new Product({name:'max 121', price: 300});
+  let data = new Product({
+    name:'max 100', 
+    price: 300, 
+    brand:'maxx', 
+    category:'mobile'
+  });
   const result = await data.save()
   console.log(data);
 }
-main()
+
+// saveInDB()
+
+const updateInDB = async () => {
+  const Product = mongoose.model('products', productSchema)
+  let data = await Product.updateOne(
+    {name:'max 100'},
+    {
+      $set:{price: 250, name:'max 110'}
+    }
+  );
+  console.log(data);
+}
+
+// updateInDB()
+
+const deleteInDB = async () => {
+  const Product = mongoose.model('products', productSchema)
+  let data = await Product.deleteOne({name:'max 121'})
+  console.log(data);
+}
+
+// deleteInDB()
+
+const findInDB = async () => {
+  const Product = mongoose.model('products', productSchema)
+  let data = await Product.find()
+  console.log(data);
+}
+
+// findInDB()
+
+const findOneInDB = async () => {
+  const Product = mongoose.model('products', productSchema)
+  let data = await Product.findOne({name:'max 110'})
+  console.log(data);
+}
+
+findOneInDB()
+
