a8054f8fc64411932c33c8e5d71563d23e8bcebf
Roger Franco <rogerfrancogalt@gmail.com>
2023-02-25 13:50:01 -0400

    GET, DELETE and PUT api method with mongoose

--- index.js
+++ index.js
@@ -11,4 +11,23 @@ app.post('/create', async (req, res) => {
   res.send(result)
 })
 
+app.get('/list', async (req, res) => {
+  let data = await Product.find()
+  res.send(data)
+})
+
+app.delete('/delete/:_id', async (req, res) => {
+  // console.log(req.params)
+  let data = await Product.deleteOne(req.params) // desse jeito (_id) será enviado a chave e o dado ex: ({_id: id})
+  res.send(data)
+})
+
+app.put('/update/:_id', async (req, res) => {
+  let data = await Product.updateOne(
+    req.params, // desse jeito (_id) será enviado a chave e o dado ex: ({_id: id})
+    {$set: req.body}
+    )
+  res.send(data)
+})
+
 app.listen(5500)
