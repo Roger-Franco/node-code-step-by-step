35e323e4121bc1f2b281c1f3035b0caf3f13092a
Roger Franco <rogerfrancogalt@gmail.com>
2023-02-27 12:52:36 -0400

    Routing add in react.js

--- e-dashboard/front-end/src/App.js
+++ e-dashboard/front-end/src/App.js
@@ -1,10 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import { Nav } from './Nav';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <h1>E-Dashboard</h1>
+      <BrowserRouter>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<h1>Product Component</h1>} />
+        <Route path="/add" element={<h1>Add Component</h1>} />
+        <Route path="/update" element={<h1>Update Component</h1>} />
+        <Route path="/logout" element={<h1>Logout</h1>} />
+        <Route path="/profile" element={<h1>Profile</h1>} />
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
