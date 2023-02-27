1aa1836a84c114b6441c8d59fcc2e0e1924cfdf7
Roger Franco <rogerfrancogalt@gmail.com>
2023-02-27 09:36:49 -0400

    update and delete API with MySQL

--- index.js
+++ index.js
@@ -24,4 +24,24 @@ app.post("/", (req, res) => {
   })
 })
 
+app.put("/:id", (req, res) => {
+  const id = req.params.id
+  const title = req.body.title
+  const pageqty = req.body.pageqty
+  const sql = `UPDATE books SET title = '${title}', pageqty = '${pageqty}' WHERE id = '${id}'`
+  conn.query(sql, (error, results, fields) => {
+    if(error) throw error;
+    res.send(results)
+  })
+})
+
+app.delete("/:id", (req, res) => {
+  const id = req.params.id
+  // const sql = `DELETE FROM books  WHERE id = '${req.params.id}'`
+  conn.query(`DELETE FROM books  WHERE id = '${req.params.id}'`, (error, results) => {
+    if(error) throw error;
+    res.send(results)
+  })
+})
+
   app.listen(5500)
