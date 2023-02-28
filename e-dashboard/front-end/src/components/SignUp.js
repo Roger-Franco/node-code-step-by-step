38bdaf875d9ed32f185f9b3456003b8e459fe04c
Roger Franco <rogerfrancogalt@gmail.com>
2023-02-28 18:59:52 -0400

    Integrate SignUp API in React

--- e-dashboard/front-end/src/components/SignUp.js
+++ e-dashboard/front-end/src/components/SignUp.js
@@ -1,12 +1,25 @@
 import React, {useState} from 'react'
+import {useNavigate} from 'react-router-dom'
+
 
 export const SignUp = () => {
   const [name, setName] = useState("")
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
+  const navigate = useNavigate()
 
-  const collectData = () => {
-    console.warm(name, email, password)
+  const collectData = async () => {
+    console.log(name, email, password)
+    let result = await fetch("http://localhost:5500/register", {
+      method: 'post',
+      body: JSON.stringify({name, email, password}),
+      headers: {
+        'Content-Type':'application/json'
+      }
+    })
+    result = await result.json()
+    console.log(result)
+    navigate('/')
   }
 
   return(
