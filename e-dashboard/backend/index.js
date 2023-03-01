1d7189b48123bcf55a4970503f2b8efec004f016
Roger Franco <rogerfrancogalt@gmail.com>
2023-03-01 17:12:46 -0400

    Login API in NodeJs

--- e-dashboard/backend/index.js
+++ e-dashboard/backend/index.js
@@ -10,7 +10,23 @@ app.use(cors())
 app.post("/register", async (req, res) => {
   let user = new User(req.body)
   let result = await user.save()
+  result = result.toObject()
+  delete result.password // essa função serve para retirar o password
   res.send(result)
+})
+
+app.post("/login", async (req, res) => {
+  if(req.body.email && req.body.password) {
+  let user = await User.findOne(req.body).select("-password")
+  // let result = await user.save()
+  if(user) {
+    res.send(user)
+  } else {
+    res.send({"result": "No user found"})
+  }
+} else {
+  res.send({"result": "You need to inform email and password"})
+}
 })
 
 app.listen(5500)
