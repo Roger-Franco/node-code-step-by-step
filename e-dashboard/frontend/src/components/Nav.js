41f821d50bd0cb89afeadf66931d931ec01d1bf8
Roger Franco <rogerfrancogalt@gmail.com>
2023-03-02 16:51:13 -0400

    React js Navbar Update

--- e-dashboard/frontend/src/components/Nav.js
+++ e-dashboard/frontend/src/components/Nav.js
@@ -14,19 +14,26 @@ export const Nav = () => {
 
   return (
     <div>
+      <img
+      className="logo"
+       src="https://i.pinimg.com/736x/33/b8/69/33b869f90619e81763dbf1fccc896d8d--lion-logo-modern-logo.jpg"
+       alt="logo-lion"
+        />
+      {
+        auth ?
       <ul className="nav-ul">
         <li> <Link to="/">Products</Link></li>
         <li> <Link to="/add">Add Products</Link></li>
         <li> <Link to="/update">Update Products</Link></li>
         <li> <Link to="/profile">Profile</Link></li>
-        {
-          auth ? <li><Link onClick={logout} to="/signup">Logout</Link></li> :
-          <>
-          <li><Link to="/signup">Sign Up</Link></li>
-          <li><Link to="/login">Login</Link></li>
-          </>
-        }
+        <li><Link onClick={logout} to="/signup">Logout ({JSON.parse(auth).name})</Link></li>
       </ul>
+        :
+      <ul className="nav-ul nav-right">
+        <li><Link to="/signup">Sign Up</Link></li>
+        <li><Link to="/login">Login</Link></li>
+      </ul>
+        }
     </div>
   )
 }
