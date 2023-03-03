46004b14835a53f722200f8bed4c2680eaa2beb0
Roger Franco <rogerfrancogalt@gmail.com>
2023-03-03 17:12:11 -0400

    React js Form validation in add product

--- e-dashboard/frontend/src/components/AddProduct.js
+++ e-dashboard/frontend/src/components/AddProduct.js
@@ -5,9 +5,13 @@ export const AddProduct = () => {
   const [price, setPrice] = useState('')
   const [category, setCategory] = useState('')
   const [company, setCompany] = useState('')
+  const [error, setError] = useState(false)
 
   const addProduct = async () => {
-    console.log(name, price, category, company);
+    if(!name || !price || !category || !company) {
+      setError(true)
+      return false
+    }
     const userId = JSON.parse(localStorage.getItem('user'))._id
     console.log(userId);
     let result = await fetch("http://localhost:5500/add-product", {
@@ -25,9 +29,13 @@ export const AddProduct = () => {
     <div className="product">
       <h1>Add Product</h1>
       <input className='inputBox'  type="text" placeholder='Enter product name' value={name} onChange={(e)=>setName(e.target.value)}/>
+      {error && !name &&<span className='invalid-input'>Enter valid name</span>}
       <input className='inputBox'  type="text" placeholder='Enter product price' value={price} onChange={(e)=>setPrice(e.target.value)}/>
+      {error && !price &&<span className='invalid-input'>Enter valid price</span>}
       <input className='inputBox'  type="text" placeholder='Enter product category' value={category} onChange={(e)=>setCategory(e.target.value)}/>
+      {error && !category &&<span className='invalid-input'>Enter valid category</span>}
       <input className='inputBox'  type="text" placeholder='Enter product company' value={company} onChange={(e)=>setCompany(e.target.value)}/>
+      {error && !company &&<span className='invalid-input'>Enter valid company</span>}
     <button onClick={addProduct} className='appButton'>Add Product</button>
     </div>
   )
