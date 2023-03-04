d910efd203f1effb13ce630e74439fb5de8f3519
Roger Franco <rogerfrancogalt@gmail.com>
2023-03-04 11:22:06 -0400

    Integrate Delete Product API

--- e-dashboard/frontend/src/components/ProductList.js
+++ e-dashboard/frontend/src/components/ProductList.js
@@ -12,7 +12,18 @@ export const ProductList = () => {
     result = await result.json()
     setProducts(result)
   }
-  console.log(products);
+
+  const deleteProduct =  async(id) => {
+    // console.log(id);
+    let result = await fetch(`http://localhost:5500/product/${id}`, {
+      method: "delete"
+    })
+    result = await result.json()
+    if(result) {
+      // alert("Product deleted")
+      getProducts()
+    }
+  }
   
   return (
     <div className='product-list'>
@@ -22,13 +33,15 @@ export const ProductList = () => {
         <li>Name</li>
         <li>Price</li>
         <li>Category</li>
+        <li>Operation</li>
       </ul>
       {products.map((item, index) => (
-        <ul key={item}>
+        <ul key={item._id}>
           <li>{index + 1}</li>
           <li>{item.name}</li>
           <li>{item.price}</li>
           <li>{item.category}</li>
+          <li><button onClick={() => deleteProduct(item._id)}>Delete</button></li>
         </ul>
       ))}
     </div>
